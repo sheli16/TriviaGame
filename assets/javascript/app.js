addd094323664891923a3f5a18c2a364268e9643
sheli16 <sheli16@live.seminolestate.edu>
2016-05-05 01:38:10 -0400

    timer display

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
    // crystalNumbers();
 
 //function computernumber = Math.floor((Math.random() * 100) + 19;
-    var Trivia = [4];
+    var Trivia = [];
     Trivia[0]= "In 1973 this singer had one of Discos earliest hits";
     Trivia[1]= "In 1975 Harold Melivin and the Blue notes had this hit";
     Trivia[2]= "In 1975 'What am I going to do with you' became this singers big hits"; 
@@ -19,15 +19,37 @@ $(document).ready(function() {
     //a5: "Last Dance" 
     console.log(Trivia[4]);
     
-    var answers = [];
-    answers[0] = "Don Downing", "Michael Jackson", "Frank Sinatra", "Sylverster";
-    answers[1] = "The Love I lost", "a", "b", "c";
-    answers[3] = "Barry White", "a", "b", "c";
-    answers[4] = "Bee Gees","a", "b", "c";
-    answers[5] = "Last Dance","a", "b", "c";
+   // var answers = [
+   //["Don Downing", "Michael Jackson", "Frank Sinatra", "Sylverster"],
+    //["The Love I lost", "a", "b", "c"],
+    //["Barry White", "a", "b", "c"],
+    //["Bee Gees","a", "b", "c"],
+    //["Last Dance","a", "b", "c"]];
    
     
+    var wronganswers = []; 
+    wronganswers[0] ="Michael Jackson";
+    wronganswers[1] ="Janet Jackson";
+    wronganswers[2] ="Donald Trump";
+    wronganswers[3] ="Kiss";
+    wronganswers[4] ="Queen";
+    wronganswers[5] ="Cool and the Gang";
+    wronganswers[6] ="Gloria Gaynor";
+    wronganswers[7] ="Daffy Duck";
+    wronganswers[8] ="Luke Skywalker";
+    wronganswers[9] ="Mr Roper";
 
+
+
+ var rightanswers = [];
+    rightanswers[0] ="Don Downing";
+    rightanswers[1] ="The Love I lost";
+    rightanswers[2] ="Barry White";
+    rightanswers[3] ="Bee Gees";
+    rightanswers[4] ="Last Dance";
+
+
+   
 $('#start').on('click', function() {
   console.log("works");
    triviaquestions();
@@ -40,12 +62,144 @@ function triviaquestions() {
   var randomquestions = Trivia[number];
     console.log(randomquestions);
     $('#Questions').html('<h4>'+ randomquestions +'</h4>');
+  
+if (number = 0 ){
+  $("#1q").html('<li><p>'+ arightnswers[0]  +'</p></li>');
+  $("#2q").html('<li><p>'+ wronganswers[2]  +'</p></li>');
+  $("#3q").html('<li><p>'+ wronganswers[3]  +'</p></li>');
+  $("#4q").html('<li><p>'+ wronganswers[4]  +'</p></li>');
+}
+  if (number = 1){
+  $("#1q").html('<li><p>'+ wronganswers[5]  +'</p></li>');
+  $("#2q").html('<li><p>'+ wronganswers[6]  +'</p></li>');
+  $("#3q").html('<li><p>'+ rightanswers[1]  +'</p></li>');
+  $("#4q").html('<li><p>'+ wronganswers[7]  +'</p></li>');
+ }
+ if (number = 2){
+  $("#1q").html('<li><p>'+ wronganswers[5]  +'</p></li>');
+  $("#2q").html('<li><p>'+ wronganswers[6]  +'</p></li>');
+  $("#3q").html('<li><p>'+ rightanswers[2]  +'</p></li>');
+  $("#4q").html('<li><p>'+ wronganswers[7]  +'</p></li>');
+}
+  if (number = 3){
+  $("#1q").html('<li><p>'+ wronganswers[5]  +'</p></li>');
+  $("#2q").html('<li><p>'+ wronganswers[6]  +'</p></li>');
+  $("#3q").html('<li><p>'+ rightanswers[3]  +'</p></li>');
+  $("#4q").html('<li><p>'+ wronganswers[7]  +'</p></li>');
+}
+if (number = 4){
+  $("#1q").html('<li><p>'+ wronganswers[5]  +'</p></li>');
+  $("#2q").html('<li><p>'+ rightanswers[4]  +'</p></li>');
+  $("#3q").html('<li><p>'+ wronganswers[6] +'</p></li>');
+  $("#4q").html('<li><p>'+ wronganswers[7]  +'</p></li>');
 
-    $("#list-group-item list-group-item-success").html('<p>'+ answers[number,0] +'</p>');
-    console.log(answers[number,0]);
-    $('#list-group-item list-group-item-info').html('<li>'+ answers[number,1] +'</li>');
-    $('#list-group-item list-group-item-warning').html('<li>'+ answers[number,2] +'</li>');
-    $('#list-group-item list-group-item-danger').html('<li>'+ answers[number,3] +'</li>');
-    
+  }
+  if (number = 5){
+  $("#1q").html('<li><p>'+ wronganswers[5]  +'</p></li>');
+  $("#2q").html('<li><p>'+ wronganswers[6]  +'</p></li>');
+  $("#3q").html('<li><p>'+ wronganswers[7] +'</p></li>');
+  $("#4q").html('<li><p>'+ rightanswers[5]  +'</p></li>');
+} 
+ };
+
+function timer(){
+var timeLeft = 30;
+var elem = document.getElementById('timer');
+var timerId = setInterval(countdown, 30000);
+
+function countdown() {
+  if (timeLeft == 0) {
+    clearTimeout(timerId);
+    triviaquestions();
+  } else {
+    timer.innerHTML = timeLeft + 'seconds';
+    timeLeft--;
+  };
+
+
 };
+
+};
+
 });
+
+
+
+//if number = 2
+   //$("#1q").html('<li><p>'+ answers[1]  +'</p></li>');
+   // console.log(answers[number,1]);
+  //  console.log(number);
+
+    //if number = 1
+  
+
+   //$("#1q").html('<li><p>'+ answers[1]  +'</p></li>');
+    //console.log(answers[number,1]);
+    //console.log(number);
+     
+     //$("#2q").html('<li><p>'+ answers[number,1] +'</p></li>');
+    //console.log(answers[number,1]);
+     // console.log(number);
+     // $("#3q").html('<li><p>'+ answers[number,2] +'</p></li>');
+    //console.log(answers[number,2]);   
+      
+    //$("#4q").html('<li><p>'+ answers[number,3] +'</p></li>');
+   // console.log(answers[number,3]);
+
+//$('#1q').on('click', function() {
+ // console.log("works");
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//function stop(){
+//   console.log('stopping');
+//   clearInterval(counter);
+// }
+//
+// function recordLap(){
+//   var converted = timeConverter(time);
+//   $('#laps').append('<p>Lap ' + lap + ' : ' + converted + '</p>');
+//   lap++;
+// }
+//
+// function count(){
+//   time++;
+//   var converted = timeConverter(time);
+//   $('#display').html(converted);
+// }
+//
+// function timeConverter(t){
+//   var minutes = Math.floor(t/60);
+//   var seconds = t - (minutes * 60);
+//   if (seconds < 10){
+//     seconds = "0" + seconds;
+//   }
+//   if (minutes === 0){
+//     minutes = "00";
+//   } else if (minutes < 10){
+//     minutes = "0" + minutes;
+//   }
+//   return minutes + ":" + seconds;
+// }
+
+
+
+
